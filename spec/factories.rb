85752559fffc938656ee23d3ab756f77d004b3f0
Matt Schwartz <matt@matthewschwartz.me>
2014-11-26 10:32:43 -0600

    readding Gemfile.lock

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,13 @@ require "factory_girl"
 
 FactoryGirl.define do
 
+  factory :stories, class: HighSnHn::Story do
+    score 10
+    comment_count 100
+    created_at (Time.now - 1.day)
+    submission
+  end
+
   factory :snapshot, class: HighSnHn::Snapshot do
     score 10
     comment_count 100
