fd16c1af247148a68309c119760d157f354f24ef
Matt Schwartz <matt@matthewschwartz.me>
2014-12-25 18:16:24 -0600

    catch a null result when GETing a top story

--- lib/high_sn_hn/workers/top_story_worker.rb
+++ lib/high_sn_hn/workers/top_story_worker.rb
@@ -8,9 +8,16 @@ module HighSnHn
       top = HighSnHn::HnTopStories.new
 
       unless top.stories.blank?
-        top.stories.map {|id| HighSnHn::HnItem.new(id).fetch }
+        top.stories.map do |id|
+          begin
+            HighSnHn::HnItem.new(id).fetch
+          rescue NullItemResult => e
+            # there was no HTTP result for the item fetch - requeue
+            LOGGER.error("#{e}")
+            HighSnHn::ReEnqueueItem.new(id)
+          end
+        end
       end
-
     rescue Exception => e
       LOGGER.error("there was some other error: #{e}")
       LOGGER.error("#{e.backtrace.join("\n")}")
